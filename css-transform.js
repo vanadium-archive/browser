0f3513033c9a7c03e731ec379198953ad0eae1ee
Ali Ghassemi <aghassemi@google.com>
2014-08-14 13:52:29 -0700

    veyron-browser: Using Rework vars and import plugins to setup theming and some common css variables like common sizes and reset CSS.
    Change-Id: I2d43dfbd1975f2e417044c7b2b6b13dc1a0024f6

--- css-transform.js
+++ css-transform.js
@@ -1,9 +1,11 @@
 var through2 = require('through2');
 var path = require('path');
+var rework = require('rework');
+var reworkVars = require('rework-vars');
+var reworkImport = require('rework-import');
 
 module.exports = transform;
 
-//TODO(aghassemi) Vanilla CSS for now. Pre-process after switch to Rework
 function transform(file) {
   if (path.extname(file) !== '.css') {
     return through2();
@@ -20,8 +22,22 @@ function transform(file) {
   }
 
   function flush(callback) {
-    var css = contents.join('');
+    var string = contents.join('');
+    var css = compile(string);
     this.push('module.exports = ' + JSON.stringify(css));
     callback();
   }
+}
+
+/* Compiles the given CSS string using rework */
+function compile(string) {
+  var css = rework(string)
+    .use(reworkImport({
+      path: 'src/components'
+    }))
+    .use(reworkVars())
+    .toString({
+      compress: true
+    });
+  return css;
 }
