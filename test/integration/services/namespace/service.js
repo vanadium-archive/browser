d94e91c2ef9a2461301b853392a8ee3ca5800a38
Ali Ghassemi <aghassemi@google.com>
2014-10-01 11:22:46 -0700

    veyron-browser: Naming asserts for the service integration tests so we get more meaningful test results in both success and failure cases, such as:
    ok 1 cottage: has a mounted name
    ok 2 cottage: mounted name matches
    ok 3 cottage: has an object name
    ok 4 cottage: object name matches
    ok 5 cottage: is a server
    ok 6 cottage: has server info
    ok 7 cottage: is globbable
    ok 8 cottage: has a signature
    
    inseatd of the default:
    
    ok 1 should be equal
    ok 2 should be equal
    ok 3 should be equal
    ok 4 should be equal
    ok 5 (unnamed assert)
    ok 6 should be equal
    ok 7 should be equal
    ok 8 (unnamed assert)
    
    
    also removing reliance on t.plan() and explicitly calling t.end()
    as it is much easier to maintain.
    Change-Id: Ic363382606cbe72850658599267dba3124095917

--- test/integration/services/namespace/service.js
+++ test/integration/services/namespace/service.js
@@ -23,12 +23,9 @@ var namespaceService =
   });
 
 test('getChildren of default namespace root', function(t) {
-  t.plan(9+9);
-
   namespaceService.getChildren().
   then(function assertResult(result) {
-    // Wait until we receive the 4 top level items,
-    // cottage, house
+    // Wait until we receive the 2 top level items: cottage, house
     var numReturnedChildren;
     result(function(children) {
       numReturnedChildren = children.length;
@@ -36,38 +33,31 @@ test('getChildren of default namespace root', function(t) {
         children = _.sortBy(children, 'mountedName');
         assertCottage(children[0]);
         assertHouse(children[1]);
+        t.end();
       }
     });
-  }).catch();
+  }).catch(t.end);
 
-  // 9 assertions
   function assertCottage(item) {
-    t.equal(item.mountedName, 'cottage');
-    t.equal(item.objectName, 'cottage');
-    t.equal(item.isServer, true);
-    t.equal(item.isGlobbable, true);
-
-    t.ok(item.serverInfo.signature);
-
-    assertMounttableServiceTypeInfo(t, item.serverInfo.typeInfo);
+    assertMountedName(t, item, 'cottage');
+    assertObjectName(t, item, 'cottage');
+    assertIsServer(t, item);
+    assertIsGlobbable(t, item);
+    assertHasSignature(t, item);
+    assertMounttableServiceTypeInfo(t, item);
   }
 
-  // 9 assertions
   function assertHouse(item) {
-    t.equal(item.mountedName, 'house');
-    t.equal(item.objectName, 'house');
-    t.equal(item.isServer, true);
-    t.equal(item.isGlobbable, true);
-
-    t.ok(item.serverInfo.signature);
-
-    assertMounttableServiceTypeInfo(t, item.serverInfo.typeInfo);
+    assertMountedName(t, item, 'house');
+    assertObjectName(t, item, 'house');
+    assertIsServer(t, item);
+    assertIsGlobbable(t, item);
+    assertHasSignature(t, item);
+    assertMounttableServiceTypeInfo(t, item);
   }
 });
 
 test('getChildren of cottage/lawn', function(t) {
-  t.plan(9+5);
-
   namespaceService.getChildren('cottage/lawn').
   then(function assertResult(result) {
     // Wait until we receive the 3 items,
@@ -79,34 +69,29 @@ test('getChildren of cottage/lawn', function(t) {
         children = _.sortBy(children, 'mountedName');
         assertBack(children[0]);
         assertSprinkler(children[2]);
+        t.end();
       }
     });
   }).catch(t.end);
 
-  // 9 assertions
   function assertSprinkler(item) {
-    t.equal(item.mountedName, 'master-sprinkler');
-    t.equal(item.objectName, 'cottage/lawn/master-sprinkler');
-    t.equal(item.isServer, true);
-    t.equal(item.isGlobbable, false);
-
-    assertUnknownServiceTypeInfo(t, item.serverInfo.typeInfo);
-
-    t.ok(item.serverInfo.signature);
+    assertMountedName(t, item, 'master-sprinkler');
+    assertObjectName(t, item, 'cottage/lawn/master-sprinkler');
+    assertIsServer(t, item);
+    assertIsNotGlobbable(t, item);
+    assertHasSignature(t, item);
+    assertUnknownServiceTypeInfo(t, item);
   }
 
-  // 5 assertions
   function assertBack(item) {
-    t.equal(item.mountedName, 'back');
-    t.equal(item.objectName, 'cottage/lawn/back');
-    t.equal(item.isServer, false);
-    t.equal(item.isGlobbable, true);
-    t.equal(item.serverInfo, null);
+    assertMountedName(t, item, 'back');
+    assertObjectName(t, item, 'cottage/lawn/back');
+    assertIsNotServer(t, item);
+    assertIsGlobbable(t, item);
   }
 });
 
 test('getChildren of rooted /localhost:8881/house/kitchen', function(t) {
-  t.plan(9+9);
 
   // 8881 is the expected root mounttable port to be running for the tests
   namespaceService.getChildren('/localhost:8881/house/kitchen').
@@ -116,9 +101,9 @@ test('getChildren of rooted /localhost:8881/house/kitchen', function(t) {
     result(function(children) {
       numReturnedChildren = children.length;
     if (numReturnedChildren === 2) {
-      children = _.sortBy(children, 'mountedName');
-      assertLightSwitch(children[0]);
-      assertSmokeDetector(children[1]);
+        children = _.sortBy(children, 'mountedName');
+        assertLightSwitch(children[0]);
+        assertSmokeDetector(children[1]);
         t.end();
       }
     });
@@ -126,82 +111,77 @@ test('getChildren of rooted /localhost:8881/house/kitchen', function(t) {
 
   // 9 assertions
   function assertLightSwitch(item) {
-    t.equal(item.mountedName, 'lights');
-    t.equal(item.objectName, '/localhost:8881/house/kitchen/lights');
-    t.equal(item.isServer, true);
-    t.equal(item.isGlobbable, false);
-
-    assertUnknownServiceTypeInfo(t, item.serverInfo.typeInfo);
-
-    t.ok(item.serverInfo.signature);
+    assertMountedName(t, item, 'lights');
+    assertObjectName(t, item, '/localhost:8881/house/kitchen/lights');
+    assertIsServer(t, item);
+    assertIsNotGlobbable(t, item);
+    assertHasSignature(t, item);
+    assertUnknownServiceTypeInfo(t, item);
   }
 
   // 9 assertions
   function assertSmokeDetector(item) {
-    t.equal(item.mountedName, 'smoke-detector');
-    t.equal(item.objectName, '/localhost:8881/house/kitchen/smoke-detector');
-    t.equal(item.isServer, true);
-    t.equal(item.isGlobbable, false);
-
-    assertUnknownServiceTypeInfo(t, item.serverInfo.typeInfo);
-
-    t.ok(item.serverInfo.signature);
+    assertMountedName(t, item, 'smoke-detector');
+    assertObjectName(t, item, '/localhost:8881/house/kitchen/smoke-detector');
+    assertIsServer(t, item);
+    assertIsNotGlobbable(t, item);
+    assertHasSignature(t, item);
+    assertUnknownServiceTypeInfo(t, item);
   }
 });
 
 test('getChildren of non-existing mounttable', function(t) {
-  t.plan(1);
-
   // Should get an error
   namespaceService.getChildren('/DoesNotExist:666/What/Ever').
   then(function shouldNotGetResult(result){
     t.fail('Should have returned an error instead of result');
   }).
   catch(function assertThereIsError(err) {
-    t.ok(err);
+    t.ok(err, 'globing non-existing mounttable correctly returns error');
+    t.end();
   });
 });
 
 test('glob uses caching', function(t) {
-  t.plan(3);
-
   mockLRUCache.reset();
 
   namespaceService.glob('house', '*').
   then(function assertNoCacheHit() {
-    // First time, no cache hit so a get call followed by a set call
-    t.notOk(mockLRUCache.lastCallWasCacheHit);
+    t.notOk(mockLRUCache.lastCallWasCacheHit,
+      'first glob call is not a cache hit');
 
     // Call second time, there should have been a cache hit
     return namespaceService.glob('house', '*');
   }).then( function assertCacheHit() {
-    t.ok(mockLRUCache.lastCallWasCacheHit);
+    t.ok(mockLRUCache.lastCallWasCacheHit,
+      'second glob call is a cache hit');
 
-    // call glob with same name, different query, there should be no cache hit
+    // Call glob with same name, different query
     return namespaceService.glob('house', 'foo*');
   }).then(function assertNoCacheHit() {
-    t.notOk(mockLRUCache.lastCallWasCacheHit);
+    t.notOk(mockLRUCache.lastCallWasCacheHit,
+      'third glob call with different query is not a cache hit');
+    t.end();
   }).catch(t.end);
 });
 
 test('getSignature uses caching', function(t) {
-  t.plan(3);
-
   mockLRUCache.reset();
 
   namespaceService.getSignature('house/alarm').then(function() {
-    // First time, no cache hit
-    t.notOk(mockLRUCache.lastCallWasCacheHit);
+    t.notOk(mockLRUCache.lastCallWasCacheHit,
+      'first getSignature call is not a cache hit');
     // Call a second time
     return namespaceService.getSignature('house/alarm');
   }).then(function() {
-    // Second time, must be a cache hit
-    t.ok(mockLRUCache.lastCallWasCacheHit);
+    t.ok(mockLRUCache.lastCallWasCacheHit,
+      'second getSignature call is a cache hit');
     // Call a different name
     return namespaceService.getSignature('house/kitchen/smoke-detector');
   }).then(function() {
-    // Different name, no cache hit
-    t.notOk(mockLRUCache.lastCallWasCacheHit);
+    t.notOk(mockLRUCache.lastCallWasCacheHit,
+      'third getSignature call to a different name is not a cache hit');
+    t.end();
   });
 });
 
@@ -217,24 +197,70 @@ test('getSignature uses caching', function(t) {
  * Test helpers
  */
 
+function assertMountedName(t, item, val) {
+  t.ok(item.mountedName, item.mountedName + ': has a mounted name');
+  t.equal(item.mountedName, val, item.mountedName + ': mounted name matches');
+}
+
+function assertObjectName(t, item, val) {
+  t.ok(item.objectName, item.mountedName + ': has an object name');
+  t.equal(item.objectName, val, item.mountedName  + ': object name matches');
+}
+
+function assertIsServer(t, item) {
+  t.equal(item.isServer, true, item.mountedName  +': is a server');
+  t.ok(item.serverInfo, item.mountedName  +': has server info');
+}
+
+function assertIsNotServer(t, item) {
+  t.equal(item.isServer, false, item.mountedName  +': is not a server');
+  t.notOk(item.serverInfo, item.mountedName  +': does not have server info');
+}
+
+function assertIsGlobbable(t, item) {
+  t.equal(item.isGlobbable, true, item.mountedName  +': is globbable');
+}
+
+function assertIsNotGlobbable(t, item) {
+  t.equal(item.isGlobbable, false, item.mountedName  +': is not globbable');
+}
+
+function assertHasSignature(t, item) {
+  t.ok(item.serverInfo.signature, item.mountedName  +': has a signature');
+}
+
 /*
  * Asserts that a ServiceTypeInfo is of predefined type of Unknown Service.
- * 4 assertions
  */
-function assertUnknownServiceTypeInfo(t, typeInfo) {
-  t.equal(typeInfo.key, 'veyron-unknown');
-  t.equal(typeInfo.typeName, 'Service');
-  t.equal(typeInfo.description, null);
-  t.ok(typeInfo.icon);
+function assertUnknownServiceTypeInfo(t, item) {
+  var typeInfo = item.serverInfo.typeInfo;
+  t.equal(typeInfo.key, 'veyron-unknown',
+    item.mountedName + ': unknown type info has the right key');
+
+  t.equal(typeInfo.typeName, 'Service',
+    item.mountedName + ': unknown type info has the type name');
+
+  t.equal(typeInfo.description, null,
+    item.mountedName + ': unknown type info does not have description');
+
+  t.ok(typeInfo.icon,
+    item.mountedName + ': unknown type info has an icon');
 }
 
 /*
  * Asserts that a ServiceTypeInfo is of predefined type of mounttable.
- * 4 assertions
  */
-function assertMounttableServiceTypeInfo(t, typeInfo) {
-  t.equal(typeInfo.key, 'veyron-mounttable');
-  t.equal(typeInfo.typeName, 'Mount Table');
-  t.ok(typeInfo.description);
-  t.ok(typeInfo.icon);
+function assertMounttableServiceTypeInfo(t, item) {
+  var typeInfo = item.serverInfo.typeInfo;
+  t.equal(typeInfo.key, 'veyron-mounttable',
+    item.mountedName + ': mounttable type info has the right key');
+
+  t.equal(typeInfo.typeName, 'Mount Table',
+    item.mountedName + ': mounttable type info has the type name');
+
+  t.ok(typeInfo.description,
+    item.mountedName + ': mounttable type info has a description');
+
+  t.ok(typeInfo.icon,
+    item.mountedName + ': mounttable type info has an icon');
 }
