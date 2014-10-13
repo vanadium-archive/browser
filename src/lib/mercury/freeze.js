18624f104df7f7930d8e9d096cb388570a806e81
Ali Ghassemi <aghassemi@google.com>
2014-10-13 12:54:21 -0700

    veyron-browser: Ensuring freezing of mutable observables into immutable ones keeps the methods that can not mutate, such as map, filter, etc.
    My previous change of freezing the result of glob() broke auto-complete
    since it expected .map() to be present but when I was freezing the objects
    I was removing evey method from it. This change ensure we keep the methods
    that can not mutate the object on it. Also ensuring thowse methods themselves
    return immutable observables.
    
    Change-Id: Ie0b3859dbf7e1b14a41f2d56d4041e88e025b494

--- src/lib/mercury/freeze.js
+++ src/lib/mercury/freeze.js
@@ -1,10 +1,40 @@
 module.exports = freeze;
 
 /*
+ * List of array methods that can't cause mutation
+ */
+var IMMUTABLE_ARRAY_METHODS = [
+'concat', 'slice', 'every', 'filter', 'forEach', 'indexOf',
+'join', 'lastIndexOf', 'map', 'reduce', 'reduceRight',
+'some', 'toString', 'toLocaleString' ];
+
+/*
  * Makes an observable into an immutable observable.
  */
 function freeze(observable) {
-  return function observe(fn) {
+
+  function immutableObservable(fn) {
     return observable(fn);
-  };
+  }
+
+  // TODO(aghassemi) Uncomment the condition when
+  // Pull Request #15 in Raynos/observ-array is merged
+  //if(observable._type === 'observ-array') {
+    copyMethods(immutableObservable, observable, IMMUTABLE_ARRAY_METHODS);
+  //}
+
+  return immutableObservable;
+}
+
+function copyMethods(target, source, methods) {
+  methods.forEach(function (name) {
+    target[name] = function() {
+      var result = source[name].apply(source, arguments);
+      // If result is an observable itself, freeze it again.
+      if( typeof result === 'function' ) {
+        result = freeze(result);
+      }
+      return result;
+    };
+  });
 }
---
+++ test/unit/lib/mercury/freeze.js
@@ -0,0 +1,38 @@
+var mercury = require('mercury');
+var test = require('prova');
+var freeze = require('../../../../src/lib/mercury/freeze');
+
+test('freeze observ-array', function(t) {
+  var mutableObsArray = mercury.array(['foo']);
+  var immutableObsArray = freeze(mutableObsArray);
+
+  assertIsImmutableArray(t, immutableObsArray);
+
+  // new observables returned by methods like map, filter are immutable
+  var mappedResult = immutableObsArray.map(function(item ) {
+    return item;
+  });
+  assertIsImmutableArray(t, mappedResult, 'result of map is immutable');
+
+  // it can be observed when source is mutated
+  immutableObsArray(function(result) {
+    t.equals(result[1], 'bar', 'observable');
+    t.end();
+  });
+  mutableObsArray.push('bar');
+
+});
+
+function assertIsImmutableArray(t, obs) {
+  // no mutable method exists
+  t.ok(obs.set === undefined, 'no set method');
+  t.ok(obs.splice === undefined, 'no splice method');
+  t.ok(obs.put === undefined, 'no put method');
+
+  // immutable methods are still there
+  ['concat', 'slice', 'every', 'filter', 'forEach', 'indexOf',
+   'join', 'lastIndexOf', 'map', 'reduce', 'reduceRight',
+   'some', 'toString', 'toLocaleString' ].forEach(function(name) {
+      t.ok(obs[name] !== undefined, name + ' method exists.');
+    });
+}
