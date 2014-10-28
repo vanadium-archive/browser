1ee04f70625b8ee740831eb77681303695c25124
Ankur <ataly@google.com>
2014-10-28 10:58:27 -0700

    "veyron-browser/scripts": Use new security model
    This CL modifies the test and webapp scripts to appropriately
    set the VEYRON_CREDENTIALS environment variable and start wsprd
    with the --new_security_model flag.
    
    Change-Id: I59540b65c662ebd4078222ad1687b0a19c41056a

--- scripts/services/run-webapp.sh
+++ scripts/services/run-webapp.sh
@@ -11,11 +11,11 @@ main() {
   local -r MOUNTTABLE_PORT_COTTAGE=5169
   local -r WSPR_PORT=8124
   local -r PROXY_PORT=5164
-  local -r VEYRON_IDENTITY_PATH="${TMPDIR}/app_identity"
+  local -r VEYRON_IDENTITY_DIR="${TMPDIR}/app_credentials_dir"
   local -r SEEK_BLESSSING=true
   local -r HTTP_PORT=9000
 
-  common::run "${MOUNTTABLE_PORT}" "${MOUNTTABLE_PORT_HOUSE}" "${MOUNTTABLE_PORT_COTTAGE}" "${WSPR_PORT}" "${PROXY_PORT}" "${VEYRON_IDENTITY_PATH}" "${SEEK_BLESSSING}"
+  common::run "${MOUNTTABLE_PORT}" "${MOUNTTABLE_PORT_HOUSE}" "${MOUNTTABLE_PORT_COTTAGE}" "${WSPR_PORT}" "${PROXY_PORT}" "${VEYRON_IDENTITY_DIR}" "${SEEK_BLESSSING}"
 
   local -r SERVE="${VEYRON_ROOT}/veyron-browser/node_modules/.bin/serve"
   "${SERVE}" "${VEYRON_ROOT}"/veyron-browser/public/. --port "${HTTP_PORT}" --compress &
