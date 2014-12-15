f322ec8cc39c881a52dca31768ad0a1fb63c713b
Ali Ghassemi <aghassemi@google.com>
2014-12-15 09:16:49 -0800

    namespace-browser: Using veyron go instead of go as this script expects gopath to be set by veyron go.
    Change-Id: Idac633f46d95fc45dfcf687475f234ea51905fe2

--- scripts/services/common.sh
+++ scripts/services/common.sh
@@ -9,7 +9,7 @@
 # TODO(aghassemi) This script is becoming too complicated and
 # big for shell scripting, switch to a JavaScript or Go implementation
 
-source "$(go list -f {{.Dir}} veyron.io/veyron/shell/lib)/shell.sh"
+source "$(veyron go list -f {{.Dir}} veyron.io/veyron/shell/lib)/shell.sh"
 
 trap 'terminate force' INT TERM
 trap 'terminate' EXIT
