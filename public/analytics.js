b7ce3e7c44d1b43e7bca921a9d7b12fec7078238
Ali Ghassemi <aghassemi@google.com>
2015-04-24 22:41:44 -0700

    namespace_browser: Separating analytics script and adding an empty one. Correct analytics script will be copied over at deployment time.
    Change-Id: Iacfdeb19496b853ac3002f4df762561297d95bfc

---
+++ public/analytics.js
@@ -0,0 +1,6 @@
+// Copyright 2015 The Vanadium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Empty analytics stub
+// This file can be overwritten to include Google Analytics if desired.
