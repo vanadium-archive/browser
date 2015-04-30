d62550a894a1fc463cb7597695ab4538ffffee45
Alex Fandrianto <alexfandrianto@google.com>
2015-04-30 14:12:13 -0700

    nsb: Ensure that namespace is set from app load
    Introduces services/state/service.js as the location to
    manage persisted application state.
    
    In this CL, the namespace index is loaded at the beginning of app.js
    with the state service. This ensures that regardless of the initial route,
    that the namespace is set to the last one the user visited.
    
    Raises https://github.com/vanadium/browser/issues/74
    Closes https://github.com/vanadium/browser/issues/48
    
    Change-Id: I0467ea6c20bf7ce9f4e87bc4afb533655c29ae4f

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,37 +3,16 @@
 // license that can be found in the LICENSE file.
 
 var browseRoute = require('./browse');
-var store = require('../lib/store');
-
-var log = require('../lib/log')('routes:index');
 
 module.exports = function(routes) {
   routes.addRoute('/', handleIndexRoute);
 };
 
 function handleIndexRoute(state, events) {
-  // TODO(aghassemi) What's the prod address?, do we even want to point to
-  // v.io by default?
-  var index = '/ns.dev.v.io:8101';
-  store.getValue('index').then(function(storedIndex) {
-    if (storedIndex) {
-      index = storedIndex;
-    }
-
-    // Redirect to browse
-    events.navigation.navigate({
-      path: browseRoute.createUrl(state.browse(), {
-        namespace: index
-      })
-    });
-  }).catch(function(err) {
-    log.warn('Unable to access stored index', err);
-
-    // Redirect to browse
-    events.navigation.navigate({
-      path: browseRoute.createUrl(state.browse(), {
-        namespace: index
-      })
-    });
+  // Send the user to the browse route with their current namespace.
+  events.navigation.navigate({
+    path: browseRoute.createUrl(state.browse(), {
+      namespace: state.browse.namespace()
+    })
   });
 }
