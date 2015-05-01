f10c14995c8df3e65b12ab84c6d154ad7dd46c0e
Alex Fandrianto <alexfandrianto@google.com>
2015-05-01 15:29:51 -0700

    nsb: Test output nit fix
    Noticed that the item.mountedName wasn't printed out in a few places.
    
    Change-Id: I8bc2a14d26117bfc42cb4e970a83f2a86ba5b164

--- test/integration/services/namespace/service.js
+++ test/integration/services/namespace/service.js
@@ -378,7 +378,8 @@ function assertServer(t, item, vals) {
     assertIsNotLeaf(t, item);
   }
 
-  t.equals(!!item.isMounttable, !!vals.isMounttable, ': is a mounttable');
+  t.equals(!!item.isMounttable, !!vals.isMounttable,
+    item.mountedName + ': is a mounttable');
 }
 
 function assertSubtableName(t, item, vals) {
@@ -399,7 +400,7 @@ function assertObjectName(t, item, val) {
 }
 
 function assertIsServer(t, item) {
-  t.ok(item.hasServer, ': is a server');
+  t.ok(item.hasServer, item.mountedName + ': is a server');
 }
 
 function assertIsLeaf(t, item) {
