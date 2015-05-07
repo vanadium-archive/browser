1dedbdd70534d358d5a866efa8b1434c4a083634
Alex Fandrianto <alexfandrianto@google.com>
2015-05-07 16:22:12 -0700

    nsb: Add Namespace Browser UI Test
    This test will be optional since it needs many components
    - Chrome WebDriver
    - Google username/password
    - Xvfb
    - maven
    
    This part of the change does the following
    - Adds a Makefile target test-ui that will run the maven command.
    - The MainPage and NamespaceBrowserUITest were added too.
      It installs the extension and checks that nodes corresponding to
      production services on ns.dev.v.io:8101 were added to the DOM.
    
    MultiPart: 3/4
    Change-Id: I64a9e624dcb83c1c25f816cf23ea1663fa7dded1

---
+++ test/ui/src/test/java/io/v/webdriver/nsbrowser/NamespaceBrowserUITest.java
@@ -0,0 +1,52 @@
+// Copyright 2015 The Vanadium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package io.v.webdriver.nsbrowser;
+
+import org.junit.Test;
+
+import io.v.webdriver.VanadiumUITestBase;
+import io.v.webdriver.commonpages.OAuthLoginPage;
+import io.v.webdriver.htmlreport.HTMLReportData;
+import io.v.webdriver.htmlreport.HTMLReporter;
+
+/**
+ * UI tests for Vanadium Namespace Browser.
+ *
+ * @author jingjin@google.com
+ */
+public class NamespaceBrowserUITest extends VanadiumUITestBase {
+  /**
+   * System property name for the test url. This will be set from the mvn command line.
+   */
+  private static final String PROPERTY_TEST_URL = "testUrl";
+
+  private static final String TEST_NAME_INIT_PROCESS = "Namespace Browser Initialization Process";
+
+  /**
+   * Tests initialization process.
+   * <p>
+   * The process includes signing into Chrome, installing Vanadium plugin, authenticating OAuth, and
+   * visiting Namespace Browser's landing page.
+   */
+  @Test
+  public void testInitProcess() throws Exception {
+    HTMLReportData reportData = new HTMLReportData(TEST_NAME_INIT_PROCESS, htmlReportsDir);
+    curHTMLReportData = reportData;
+
+    super.installExtension(reportData);
+
+    // Namespace browser.
+    String url = System.getProperty(PROPERTY_TEST_URL);
+    System.out.printf("Url: %s\n", url);
+    MainPage mainPage = new MainPage(driver, url, reportData);
+    OAuthLoginPage oauthLoginPage = mainPage.goToPage();
+    oauthLoginPage.login();
+    mainPage.load();
+
+    // Write html report.
+    HTMLReporter reporter = new HTMLReporter(reportData);
+    reporter.generateReport();
+  }
+}
